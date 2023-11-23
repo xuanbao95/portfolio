3b59010395bf95b378c3d40108ad42f14a907054
bao95 <bao.luugx@upos.vn>
2023-11-23 11:37:07 +0700

    commit

--- app/pages/body/components/carousel/index.tsx
+++ app/pages/body/components/carousel/index.tsx
@@ -1,11 +1,11 @@
 'use client'
 import React from "react";
-import WaterWave from 'react-water-wave';
+import WaterEffect from 'react-water-wave';
 import avatar from "../../../../../public/avatar.png"
 export const Carousel = ()=>{
     console.log('avatar', avatar)
     return(
-        <WaterWave
+        <WaterEffect
             // imageUrl={"https://images.unsplash.com/photo-1682687220795-796d3f6f7000?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
             imageUrl={"/avatar.png"}
             style={{ width: '100%', height: '100%', backgroundSize: 'cover' }}
@@ -16,7 +16,7 @@ export const Carousel = ()=>{
                     12312
                 </div>
             )}
-        </WaterWave>
+        </WaterEffect>
 
     )
 }
